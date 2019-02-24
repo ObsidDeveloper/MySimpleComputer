8f39aba55d412279b2787421b13490efc718f253
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-02-24 18:07:51 +0300

    added changes l_lflag of struct termios

--- ReadKey.c
+++ ReadKey.c
@@ -6,6 +6,21 @@
 #include "mta/MyTerminal.h"
 #include "rk/ReadKey.h"
 
+struct termios const _term_st;
+struct termios term;
+
 int rk_myTermRegine(struct termios const *current, int regime, int vtime, int vmin, int echo, int sigint) {
 	if (CHECK_REG_ECH_SIG) return -1;
+	
+	struct termios newstate = *current;
+
+	if (regime) newstate.c_lflag |= ICANOW;
+		else newstate.c_lflag &= ~ICANOW;
+
+	if (echo) newstate.c_lflag |= ECHO;
+		else newstate.c_lflag &= ~ECHO;
+
+	if (sigint) newstate.c_lflag |= ISIG;
+		else newstate.c_lflag &= ~ISIG;
+	return 0;
 }
