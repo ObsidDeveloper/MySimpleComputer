da734b92c07986a23c0a2876326a5495e06a091c
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-02-28 17:51:57 +0300

    begin writing translate

---
+++ sat/translate.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+
+#include "SimpleTranslator.h"
+
+int * translate(int fd, int *size) {
+	char *SAcode = NULL;
+	
+}
