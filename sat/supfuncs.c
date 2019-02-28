c43bf77e4f0e14f2ef664f9d78ee94dd97333007
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-02-28 18:56:43 +0300

    writted func getlogos

--- sat/supfuncs.c
+++ sat/supfuncs.c
@@ -1,5 +1,19 @@
 #include <string.h>
 
-int checkSyntax(char *str, int remainspace) {
+int checkSyntax(char *str, int remainspace, int currentstage) {
 	if (str == NULL) return -1;
+}
+
+char * getlogos(char *str, int remainspace) {
+	char * logos = (char *)malloc(8);
+	int i = 0;
+	while (i < 8 && str[i] != ' ' && str[i] != 10) {
+		if (i == remainspace) {
+			free(logos);
+			return NULL;
+		}
+		logos[i] = *str;
+		str++;
+	}
+	return logos;
 }
