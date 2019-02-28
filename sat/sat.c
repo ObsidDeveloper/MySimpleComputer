2b1b04e651f10c2715b6a53ea44cc1026eb8e870
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-02-28 17:39:40 +0300

    added getting int-mas and writing binary file

--- sat/sat.c
+++ sat/sat.c
@@ -2,6 +2,8 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+#include "SimpleAssembler.h"
+
 int sat(char *source, char *bin_filename) {
 	if (source == NULL) {
 		printf("sat error: \n");
@@ -13,6 +15,22 @@ int sat(char *source, char *bin_filename) {
 		return -1;
 	}
 	int size_ofprogram;
-	int *bin = translate
+	int *bin = translate(descriptor, &size_ofprogram);
+
+	if (bin == NULL) {
+		printf("translator return value -1\n");
+		return -1;
+	}
+
+	FILE *binfile = fopen(bin_filename, "wb");
+	if (binfile) {
+		fwrite(bin, sizeof(int), size_ofprogram, binfile);
+	} else {
+		printf("Cannot writing binary file\n");
+		fclose(binfile);
+		return -1;
+	}
+	fclose(binfile);
+	liberation(bin);
 	return 0;
 }
