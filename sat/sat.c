01358d03e199229828872f22749bb5d0045859a1
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-02-28 17:25:41 +0300

    added checking file from adress %source

--- sat/sat.c
+++ sat/sat.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 int sat(char *source, char *bin_filename) {
 	if (source == NULL) {
 		printf("sat error: \n");
 		return -1;
 	}
-
+	int descriptor;
+	if ((descriptor = open(source, O_RDONLY) == -1) {
+		printf("sat error: %s - no such file\n", source);
+		return -1;
+	}
+	int size_ofprogram;
+	int *bin = translate
 	return 0;
 }
