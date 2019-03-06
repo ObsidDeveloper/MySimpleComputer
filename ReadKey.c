249afee893c89b725f64ec9e9b5a9d611b6c71ee
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-03-06 18:48:20 +0300

    copy new func readkey

--- ReadKey.c
+++ ReadKey.c
@@ -38,4 +38,77 @@ int rk_myTermRegine(struct termios const *current, int regime, int vtime, int vm
 	return 0;
 }
 
+int rk_readKey(enum keys *key) {
+	int term = open(TERM, O_RDWR);
+	char buf;
+
+	struct termios termState = __term_state;
+	rk_myTermRegime(&termState, 0, 1, 1, 0, 0);
+	read(term, &buf, 1);
+
+    switch (buf) {
+		case 'q':
+			*key = EXIT;
+			break;
+		case 'l':
+			*key = LOAD;
+			break;
+		case 's':
+			*key = SAVE;
+			break;
+		case 'r':
+			*key = RUN;
+			break;
+		case 't':
+			*key = STEP;
+			break;
+		case 'i':
+			*key = RESET;
+			break;
+		case 'e':
+			*key = EDIT;
+			break;
+		case '\E': {
+			read (term, &buf, 1);
+			read (term, &buf, 1);
+			switch (buf) {
+				case 65:
+					*key = UP;
+					break;
+				case 66:
+					*key = DOWN;
+					break;
+				case 67:
+					*key = RIGHT;
+					break;
+				case 68:
+					*key = LEFT;
+					break;
+				case '1':
+					read(term, &buf, 1);
+					if (buf == '5') {
+						read (term, &buf, 1);
+						if (buf == '~') *key = F5;
+					}
+					if (buf == '7') {
+						read (term, &buf, 1);
+						if (buf == '~') *key = F6;
+					}
+					break;
+				default:
+					*key = NONE;
+			}
+			break;
+			break;
+		}
+		default:
+			*key = NONE;
+			break;
+    }
+    rk_myTermRegime(&termState, 1, 0, 0, 1, 1);
+    close (term);
+
+	return 0;
+}
+
 /*Thank you, Mioko!*/
