df272e3abe1396a8a2858fd008e8045c4d14fc37
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-03-09 19:25:23 +0300

    added drawAll()

--- console/console.c
+++ console/console.c
@@ -6,6 +6,18 @@
 #include "../ReadKey.h"
 #include "console.h"
 
+int drawAll() {
+	creatBlock(&display_mem, 2);
+	displayMemory(&display_mem);
+	
+	creatBlock(&display_accum, 2);
+	creatBlock(&display_instr_count, 2);
+	creatBlock(&display_opers, 2);
+	creatBlock(&display_keys, 2);
+	creatBlock(&display_bigchar, 2);
+	
+}
+
 int displayMemory(struct block_info *mem_block) {
 	int i = 0;
 	int x = mem_block->x + 1;
@@ -17,7 +29,7 @@ int displayMemory(struct block_info *mem_block) {
 		sc_memoryGet(i, &value);
 		printNumber(x, y, value, DEFAULT);
 		i++;
-		y++;
+		y += 6;
 		if (i/10) x++;
 	}
 	return 0;
