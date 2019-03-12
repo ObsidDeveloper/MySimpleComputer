2e3a2c0a4a994109992f8b757d8c410d95736611
ObsidDeveloper <KuzKoSuh@mail.ru>
2019-03-12 18:06:09 +0300

    writed memMoveMent()

--- console/conmanage.c
+++ console/conmanage.c
@@ -18,14 +18,40 @@ int console(void) {
 }
 
 int memMovement(enum Keys key) {
+	/*TODO::WRITE DEFINES OF MSC COLORS - ALL!!!!*/
 	switch (key) {
-	case LEFT: {
-		if (InstrCount % 10) {
-			highlightCell(InstrCount, DEFAULT);
-			InstrCount--;
-			highlightCell(InstrCounter, RED);
+	     case LEFT: {
+		     if (InstrCount > 0x0) {
+			     highlightCell(InstrCount, DEFAULT);
+			     InstrCount--;
+			     highlightCell(InstrCounter, RED);
+		     }
+		  return 0;
+	     }
+	     case RIGHT: {
+			 if (InstrCount < N - 1) {
+				 highlightCell(InstrCount, DEFAULT);
+				 InstrCount++;
+				 highlightCell(InstrCount, RED);
+			 }
+			 return 0;
+		 }
+		case UP{
+			if (InstrCount / 10) {/*REMEMBER: 9/10 == 0*/
+				highlightCell(InstrCount, DEFAULT);
+				InstrCount -= 10;
+				highlightCell(InstrCount, RED);
+			}
+		    return 0;
+		}
+		case DOWN: {
+			if (InstrCount < (N - M)) {
+				highlightCell(InstrCount, DEFAULT);
+				InstrCount += 10;
+				highlightCell(InstrCount, RED);
+			}
+			return 0;
 		}
 	}
-	}
-	return 0;
+	return -1;
 }
